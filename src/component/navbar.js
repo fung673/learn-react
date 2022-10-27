35e3d93c88e8f62df818ee58f8e1806bdb2df372
unknown <alexfung1234@gmail.com>
2022-10-27 16:43:31 +0800

    update build for gh-pages

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
  return (
   <nav>
    <img src={require("../images/icon.png")} className="nav-logo" />
-   <h3 className="nav-facts">React facts</h3>
+   <h3 className="nav-facts">React Facts</h3>
    <h4 className="nav-course">React Course</h4>
   </nav>
  )
